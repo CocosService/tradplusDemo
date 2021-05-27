262601fde170c067ccef6cd2b07359e954c2c341
Zhexuan Chen <2915234902@qq.com>
2021-05-27 18:28:27 +0800

    Add startup scene

---
+++ assets/startup.ts
@@ -0,0 +1,13 @@
+import { _decorator, Component, Node, director, Event } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Startup')
+export class Startup extends Component {
+  start() {
+    // @ts-ignore
+    cc.debug.setDisplayStats(false); // 不显示左下角的FPS等调试信息
+
+    // 启用测试模式，仅测试时使用，上线前请注释掉
+    tradplus.tradPlusService.setEnableTestMode(true);
+  }
+}
