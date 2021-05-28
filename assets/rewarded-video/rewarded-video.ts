c3389fd1e8bb34d3d937cf6ad24b8804928b46e6
Zhexuan Chen <2915234902@qq.com>
2021-05-28 16:19:04 +0800

    [rewarded video] Add the demo of Rewarded Video

---
+++ assets/rewarded-video/rewarded-video.ts
@@ -0,0 +1,97 @@
+import { _decorator, Component } from 'cc';
+import { Console } from '../prefabs/console';
+const { ccclass, property } = _decorator;
+
+@ccclass('RewardedVideo')
+export class RewardedVideo extends Component {
+  @property({ type: Console })
+  console: Console = null!;
+
+  private rewardedVideo: tradplus.RewardedVideo | null = null;
+
+  start() {
+    this.console.log('Setup Rewarded Video...');
+
+    const adUnitId = '39DAC7EAC046676C5404004A311D1DB1';
+
+    this.rewardedVideo = new tradplus.RewardedVideo(adUnitId, true);
+
+    this.rewardedVideo.entryAdScenario('177010A4403105');
+
+    const customMap = {
+      user_gender: 'male',
+      user_level: '10',
+    };
+
+    // App Scope, for all placement.
+    // tradplus.tradPlusService.initCustomMap(customMap);
+    // Only for the specify placement, override the App scope's one.
+    tradplus.tradPlusService.initPlacementCustomMap(adUnitId, customMap);
+
+    this.rewardedVideo.setAdListener({
+      onAdLoaded: (adInfo: tradplus.AdInfo) => {
+        this.console.log('onAdLoaded, adInfo:', adInfo);
+      },
+
+      onAdClicked: (adInfo: tradplus.AdInfo) => {
+        this.console.log('onAdClicked, adInfo:', adInfo);
+      },
+
+      onAdImpression: (adInfo: tradplus.AdInfo) => {
+        this.console.log('onAdImpression, adInfo:', adInfo);
+      },
+
+      onAdFailed: (adError: tradplus.AdError) => {
+        this.console.log('onAdFailed, adError:', adError);
+      },
+
+      onAdClosed: (adInfo: tradplus.AdInfo) => {
+        this.console.log('onAdClosed, adInfo:', adInfo);
+      },
+
+      onAdReward: (adInfo: tradplus.AdInfo) => {
+        this.console.log('onAdReward, adInfo:', adInfo);
+      },
+    });
+
+    this.rewardedVideo.setAllAdLoadListener({
+      onAdAllLoaded: (anyAdsLoadSucceeded: boolean) => {
+        this.console.log(
+          'onAdAllLoaded, any Ads load succeeded?:',
+          anyAdsLoadSucceeded
+        );
+      },
+
+      oneLayerLoadFailed: (
+        adError: tradplus.AdError,
+        adInfo: tradplus.AdInfo
+      ) => {
+        this.console.log(
+          'oneLayerLoadFailed, adError:',
+          adError,
+          'adInfo:',
+          adInfo
+        );
+      },
+
+      oneLayerLoaded: (adInfo: tradplus.AdInfo) => {
+        this.console.log('oneLayerLoaded, adInfo:', adInfo);
+      },
+    });
+  }
+
+  showAd() {
+    if (!this.rewardedVideo) return; // Show not return.
+
+    if (this.rewardedVideo.ready) {
+      this.console.log('Show Ad');
+      this.rewardedVideo.showAd('');
+    } else {
+      this.console.log('No Ad is ready right now, please wait...');
+    }
+  }
+
+  onDestroy() {
+    this.rewardedVideo?.destroyAd();
+  }
+}
